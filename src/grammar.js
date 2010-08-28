6cfb922b0a1ffa2dc6002f8b2e1b1dce5dea3c1a
Corey Burrows <corey.burrows@centro.net>
2010-08-28 15:54:28 -0500

    Converts output of parser to a JSON based abstract syntax tree.

--- src/grammar.js
+++ src/grammar.js
@@ -15,11 +15,11 @@ var grammar = {
   ],
 
   Body: [
-    o('',                           '$$ = Bully.Nodes.Body.create();'),
-    o('Expression',                 '$$ = Bully.Nodes.Body.wrap([$1]);'),
-    o('Statement',                  '$$ = Bully.Nodes.Body.wrap([$1]);'),
-    o('Body Terminator Expression', '$1.push($3);'),
-    o('Body Terminator Statement',  '$1.push($3);'),
+    o('',                           "$$ = {type: 'Body', lines: []};"),
+    o('Expression',                 "$$ = {type: 'Body', lines: [$1]};"),
+    o('Statement',                  "$$ = {type: 'Body', lines: [$1]};"),
+    o('Body Terminator Expression', "$1.lines.push($3);"),
+    o('Body Terminator Statement',  "$1.lines.push($3);"),
     o('Body Terminator')
   ],
 
@@ -44,45 +44,45 @@ var grammar = {
   ],
 
   Constant: [
-    o('CONSTANT', '$$ = Bully.Nodes.Constant.create($1);'),
+    o('CONSTANT', "$$ = {type: 'Constant', name: $1};"),
   ],
 
   Self: [
-    o('SELF', '$$ = Bully.Nodes.Self.create();'),
+    o('SELF', "$$ = {type: 'Self'}"),
   ],
 
   Return: [
-    o('RETURN Expression', '$$ = Bully.Nodes.Return.create([$2]);'),
-    o('RETURN',            '$$ = Bully.Nodes.Return.create();')
+    o('RETURN Expression', "$$ = {type: 'Return', expression: $2};"),
+    o('RETURN',            "$$ = {type: 'Return', expression: null};")
   ],
 
   Literal: [
-    o('NUMBER', '$$ = Bully.Nodes.Literal.create("NUMBER", $1);'),
-    o('STRING', '$$ = Bully.Nodes.Literal.create("STRING", $1);'),
-    o('NIL',    '$$ = Bully.Nodes.Literal.create("NIL");'),
-    o('TRUE',   '$$ = Bully.Nodes.Literal.create("TRUE");'),
-    o('FALSE',  '$$ = Bully.Nodes.Literal.create("FALSE");')
+    o('NUMBER', "$$ = {type: 'NumberLiteral', value: $1};"),
+    o('STRING', "$$ = {type: 'StringLiteral', value: $1};"),
+    o('NIL',    "$$ = {type: 'NilLiteral'};"),
+    o('TRUE',   "$$ = {type: 'TrueLiteral'};"),
+    o('FALSE',  "$$ = {type: 'FalseLiteral'};")
   ],
 
   Call: [
-    o('IDENTIFIER',                          '$$ = Bully.Nodes.Call.create(null, $1, null);'),
-    o('IDENTIFIER ( ArgList )',              '$$ = Bully.Nodes.Call.create(null, $1, $3);'),
-    o('Expression . IDENTIFIER',             '$$ = Bully.Nodes.Call.create($1, $3, null);'),
-    o('Expression . IDENTIFIER ( ArgList )', '$$ = Bully.Nodes.Call.create($1, $3, $5);')
+    o('IDENTIFIER',                          "$$ = {type: 'Call', expression: null, name: $1, argList: null};"),
+    o('IDENTIFIER ( ArgList )',              "$$ = {type: 'Call', expression: null, name: $1, argList: $3};"),
+    o('Expression . IDENTIFIER',             "$$ = {type: 'Call', expression: $1,   name: $3, argList: null};"),
+    o('Expression . IDENTIFIER ( ArgList )', "$$ = {type: 'Call', expression: $1,   name: $3, argList: $5};")
   ],
 
   If: [
     o('IfStart END'),
-    o('IfStart ELSE NEWLINE Body END', '$1.addElse($4.needsReturn());'),
+    o('IfStart ELSE NEWLINE Body END', '$1.elseBody = $4;'),
   ],
 
   IfStart: [
-    o('IF Expression Then Body', '$$ = Bully.Nodes.If.create([$2, $4.needsReturn()]);'),
-    o('IfStart ElsIf',           '$1.push($2);')
+    o('IF Expression Then Body', "$$ = {type: 'If', conditions: [$2], bodies: [$4], elseBody: null};"),
+    o('IfStart ElsIf',           "$1.conditions = $1.conditions.concat($2.conditions); $1.bodies = $1.bodies.concat($2.bodies);")
   ],
 
   ElsIf: [
-    o('ELSIF Expression Then Body', '$$ = Bully.Nodes.If.create([$2, $4.needsReturn()]);')
+    o('ELSIF Expression Then Body', "$$ = {type: 'If', conditions: [$2], bodies: [$4], elseBody: null};")
   ],
 
   Then: [
@@ -92,51 +92,51 @@ var grammar = {
   ],
 
   ArgList: [
-    o('',                     '$$ = Bully.Nodes.ArgList.create();'),
-    o('Expression',           '$$ = Bully.Nodes.ArgList.create([$1]);'),
-    o('ArgList , Expression', '$1.push($3);')
+    o('',                     "$$ = {type: 'ArgList', expressions: []};"),
+    o('Expression',           "$$ = {type: 'ArgList', expressions: [$1]};"),
+    o('ArgList , Expression', "$1.expressions.push($3);")
   ],
 
   Def: [
-    o('DEF IDENTIFIER Terminator Body END',               '$$ = Bully.Nodes.Def.create($2, Bully.Nodes.ParamList.create(), $4.needsReturn());'),
-    o('DEF IDENTIFIER ( ParamList ) Terminator Body END', '$$ = Bully.Nodes.Def.create($2, $4, $7.needsReturn());'),
+    o('DEF IDENTIFIER Terminator Body END',               "$$ = {type: 'Def', name: $2, params: null, body: $4};"),
+    o('DEF IDENTIFIER ( ParamList ) Terminator Body END', "$$ = {type: 'Def', name: $2, params: $4,   body: $7};")
   ],
 
   ParamList: [
-    o('',                                         '$$ = Bully.Nodes.ParamList.create();'),
-    o('ReqParamList',                             '$$ = Bully.Nodes.ParamList.create([$1]);'),
-    o('OptParamList',                             '$$ = Bully.Nodes.ParamList.create([$1]);'),
-    o('SplatParam',                               '$$ = Bully.Nodes.ParamList.create([$1]);'),
-    o('ReqParamList , OptParamList',              '$$ = Bully.Nodes.ParamList.create([$1, $3]);'),
-    o('ReqParamList , OptParamList , SplatParam', '$$ = Bully.Nodes.ParamList.create([$1, $3, $5]);'),
-    o('ReqParamList , SplatParam',                '$$ = Bully.Nodes.ParamList.create([$1, $3]);'),
-    o('OptParamList , SplatParam',                '$$ = Bully.Nodes.ParamList.create([$1, $3]);')
+    o('',                                         "$$ = {type: 'ParamList', required: null, optional: null, splat: null};"),
+    o('ReqParamList',                             "$$ = {type: 'ParamList', required: $1,   optional: null, splat: null};"),
+    o('OptParamList',                             "$$ = {type: 'ParamList', required: null, optional: null, splat: null};"),
+    o('SplatParam',                               "$$ = {type: 'ParamList', required: null, optional: null, splat: null};"),
+    o('ReqParamList , OptParamList',              "$$ = {type: 'ParamList', required: $1,   optional: $3,   splat: null};"),
+    o('ReqParamList , OptParamList , SplatParam', "$$ = {type: 'ParamList', required: $1,   optional: $3,   splat: $5};"),
+    o('ReqParamList , SplatParam',                "$$ = {type: 'ParamList', required: $1,   optional: null, splat: $3};"),
+    o('OptParamList , SplatParam',                "$$ = {type: 'ParamList', required: null, optional: $1,   splat: $3};")
   ],
 
   ReqParamList: [
-    o('IDENTIFIER',                '$$ = Bully.Nodes.ReqParamList.create($1);'),
-    o('ReqParamList , IDENTIFIER', '$1.push($3);')
+    o('IDENTIFIER',                "$$ = {type: 'ReqParamList', names: [$1]};"),
+    o('ReqParamList , IDENTIFIER', "$1.names.push($3);")
   ],
 
   OptParamList: [
-    o('IDENTIFIER = Expression',                '$$ = Bully.Nodes.OptParamList.create($1, $3);'),
-    o('OptParamList , IDENTIFIER = Expression', '$1.push($3, $5);')
+    o('IDENTIFIER = Expression',                "$$ = {type: 'OptParamList', params: [[$1, $3]]};"),
+    o('OptParamList , IDENTIFIER = Expression', "$1.params.push([$3, $5]);")
   ],
 
   SplatParam: [
-    o('* IDENTIFIER', '$$ = Bully.Nodes.SplatParam.create($2);')
+    o('* IDENTIFIER', "$$ = {type: 'SplatParam', name: $2};")
   ],
 
   Assignment: [
-    o('IDENTIFIER = Expression',     '$$ = Bully.Nodes.LocalAssign.create($1, $3);'),
-    o('@ IDENTIFIER = Expression',   '$$ = Bully.Nodes.InstanceAssign.create($2, $4);'),
-    o('@ @ IDENTIFIER = Expression', '$$ = Bully.Nodes.ClassAssign.create($3, $5);'),
-    o('CONSTANT = Expression',       '$$ = Bully.Nodes.ConstantAssign.create($1, $3);')
+    o('IDENTIFIER = Expression',     "$$ = {type: 'LocalAssign', name: $1, expression: $3};"),
+    o('@ IDENTIFIER = Expression',   "$$ = {type: 'InstanceAssign', name: $2, expression: $4};"),
+    o('@ @ IDENTIFIER = Expression', "$$ = {type: 'ClassAssign', name: $3, expression: $5};"),
+    o('CONSTANT = Expression',       "$$ = {type: 'ConstantAssign', name: $1, expression: $3};")
   ],
 
   Class: [
-    o('CLASS CONSTANT Terminator Body END',            '$$ = Bully.Nodes.Class.create($2, null, [$4]);'),
-    o('CLASS CONSTANT < CONSTANT Terminator Body END', '$$ = Bully.Nodes.Class.create($2, $4, [$6]);')
+    o('CLASS CONSTANT Terminator Body END',            "$$ = {type: 'Class', constant: $2, super: null, body: $4};"),
+    o('CLASS CONSTANT < CONSTANT Terminator Body END', "$$ = {type: 'Class', constant: $2, super: $4, body: $6};")
   ]
 };
 
