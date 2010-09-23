68ccf901b7349971d0bfb98da5b2ed9194bdeb97
Corey Burrows <corey.burrows@centro.net>
2010-09-23 07:39:25 -0500

    Allow the keyword 'class' to be used as a method name.

--- src/rewriter.js
+++ src/rewriter.js
@@ -1,34 +1,42 @@
 
 Bully.Rewriter = function(tokens) {
   this.tokens = tokens;
-  this.index  = -1;
+  this.index  = 0;
   return this;
 };
+
+Bully.Rewriter.KEYWORDS_ALLOWED_AS_METHODS = [ 'CLASS' ];
 
 Bully.Rewriter.prototype = {
   rewrite: function() {
     this.remove_extra_newlines();
+    this.convert_keyword_method_calls();
     return this.tokens;
   },
 
   next: function() {
+    var t = this.tokens[this.index];
     this.index += 1;
+    return t;
+  },
+
+  peak: function() {
     return this.tokens[this.index];
   },
 
   reset: function() {
-    this.index = -1;
+    this.index = 0;
   },
 
   remove_next_of_type: function(type) {
-    while (this.tokens[this.index + 1][0] === type) {
-      this.tokens.splice(this.index + 1, 1);
+    while (this.tokens[this.index][0] === type) {
+      this.tokens.splice(this.index, 1);
     }
   },
 
   remove_prev_of_type: function(type) {
-    while (this.tokens[this.index - 1][0] === type) {
-      this.tokens.splice(this.index - 1, 1);
+    while (this.tokens[this.index - 2][0] === type) {
+      this.tokens.splice(this.index - 2, 1);
       this.index -= 1;
     }
   },
@@ -50,6 +58,17 @@ Bully.Rewriter.prototype = {
     }
 
     this.reset();
+  },
+
+  convert_keyword_method_calls: function() {
+    var t1, t2;
+
+    while ((t1 = this.next()) && (t2 = this.peak())) {
+      if ((t1[0] === '.' || t1[0] === 'DEF') &&
+          Bully.Rewriter.KEYWORDS_ALLOWED_AS_METHODS.indexOf(t2[0]) != -1) {
+        t2[0] = 'IDENTIFIER';
+      }
+    }
   }
 };
 
