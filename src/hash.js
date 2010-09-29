4f45c63c03fce2d1ab18112c0a16a63a5e7f84b3
Corey Burrows <corey.burrows@centro.net>
2010-09-29 08:51:10 -0500

    Adding Hash#keys, Hash#values, and Hash#inspect.

--- src/hash.js
+++ src/hash.js
@@ -1,8 +1,14 @@
-Bully.hash_new = function(js_obj) {
-  return Bully.make_object(js_obj || {}, Bully.Hash);
+Bully.hash_new = function() {
+  var h = Bully.make_object({}, Bully.Hash);
+  Bully.ivar_set(h, '__keys__', []);
+  return h;
 };
 
 Bully.hash_set = function(hash, key, value) {
+  var keys = Bully.ivar_get(hash, '__keys__');
+
+  if (keys.indexOf(key) === -1) { keys.push(key); }
+
   key = Bully.dispatch_method(key, 'hash');
   hash[key] = value;
   return value;
@@ -27,4 +33,30 @@ Bully.init_hash = function() {
   Bully.define_method(Bully.Hash, '[]', function(self, args) {
     return Bully.hash_get(self, args[0]);
   }, 1, 1);
+
+  Bully.define_method(Bully.Hash, 'keys', function(self, args) {
+    return Bully.array_new(Bully.ivar_get(self, '__keys__'));
+  });
+
+  Bully.define_method(Bully.Hash, 'values', function(self, args) {
+    var keys = Bully.ivar_get(self, '__keys__'), values = [], i;
+
+    for (i = 0; i < keys.length; i++) {
+      values.push(Bully.hash_get(self, keys[i]));
+    }
+
+    return Bully.array_new(values);
+  });
+
+  Bully.define_method(Bully.Hash, 'inspect', function(self, args) {
+    var keys = Bully.ivar_get(self, '__keys__'), elems = [], i, s;
+
+    for (i = 0; i < keys.length; i++) {
+      s = Bully.dispatch_method(keys[i], 'inspect').data + ' => ';
+      s += Bully.dispatch_method(Bully.hash_get(self, keys[i]), 'inspect').data;
+      elems.push(s);
+    }
+
+    return Bully.str_new('{' + elems.join(', ') + '}');
+  });
 };
